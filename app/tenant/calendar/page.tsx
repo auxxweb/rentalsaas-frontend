f80b4b9f832dbc9973b2ef2c94b82baa70661d25
auxxweb <auxxweb@gmail.com>
2026-04-10 01:31:14 +0530

    vite change commit

--- app/tenant/calendar/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import SerialCalendarView from '@/components/calendar/SerialCalendarView'
-
-export default function TenantCalendarPage() {
-  return <SerialCalendarView bookingsPath="/tenant/bookings" />
-}
