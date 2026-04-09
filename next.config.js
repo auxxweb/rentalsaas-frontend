dedcc789a810d9fde22fd63b3d940ec8e7edd222
auxxweb <auxxweb@gmail.com>
2026-04-09 23:49:33 +0530

    third commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://server.rentalsaas.auxxbay.com/api',
+    // NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
   },
   experimental: {
     optimizePackageImports: ['date-fns'],
